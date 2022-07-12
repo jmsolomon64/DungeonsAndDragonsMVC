47e68fc0245e34800f3bd381833d57fb758f143d
Jacob Solomon <jmsolomon64@gmail.com>
2022-07-12 18:24:39 -0400

    Initial Entity and Models creation for equipment

---
+++ DungeonsAndDragons.Data/Entity/Equipment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonsAndDragons.Data.Entity
+{
+    public class Equipment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public double Weight { get; set; }
+
+        [Required]
+        public double Cost { get; set; }
+
+        [Required]
+        [MaxLength(10000)]
+        public string Description { get; set; }
+
+        public virtual ICollection<Character> Characters { get; set; }
+
+        public Equipment()
+        {
+            Characters = new List<Character>();
+        }
+    }
+}
