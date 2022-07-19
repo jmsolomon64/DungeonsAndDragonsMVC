4daa4eda2b627c83c328bce8e56fd2e027deaa67
Jacob Solomon <jmsolomon64@gmail.com>
2022-07-19 18:40:00 -0400

    blazor init

--- DungeonsAndDragons.MVC/Program.cs
+++ DungeonsAndDragons.MVC/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+builder.Services.AddServerSideBlazor();
 
 var app = builder.Build();
 
@@ -39,6 +40,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapBlazorHub();
 app.MapRazorPages();
 
 app.Run();
